7f093b24df4dc2b4249dc184b5b557f0400cb692
mshestakova <imfamil105@gmail.com>
2018-12-28 15:01:28 +0200

    user changed

--- remybot/src/main/java/models/User.java
+++ remybot/src/main/java/models/User.java
@@ -1,4 +1,80 @@
 package models;
 
+import dao.ObjectDao;
+import dao.ParamDao;
+import entities.Param;
+import entities.Object;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private Long userId;
+    private String userName;
+    private boolean isAdmin;
+
+
+    private static ObjectDao objectDao = new ObjectDao();
+    private static ParamDao paramDao = new ParamDao();
+    private static List<Object> objects;
+    private static List<Param> params;
+
+    public User(Long id, String name, boolean admin)
+    {
+        userId = id;
+        userName = name;
+        isAdmin = admin;
+    }
+
+    public static List<User> getAllUsers()
+    {
+        objects = objectDao.getAll("http://localhost:8080/botapi/objects");
+        params = paramDao.getAll("http://localhost:8080/botapi/params");
+        List<User> users = new ArrayList<>();
+        for (Object obj : objects)
+        {
+            if(obj.getObject_type_id() == 1)
+            {
+                User user = new User(obj.getObject_id(), obj.getName(), false);
+                for (Param param : params)
+                {
+                    if(param.getAttr_id() == 1 && param.getObject_id().equals(user.getUserId()))
+                    {
+                        user.setAdmin(param.getBool_value() == 1);
+                    }
+                }
+                users.add(user);
+            }
+        }
+        return users;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return this.userName;
+    }
 }
